9ddcc30628ced7f07279fd06e5a384dd470d95b5
miraditasstudiocitas-debug <miraditasstudiocitas@gmail.com>
2026-06-11 19:02:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -406,7 +406,7 @@ const EQUIPO = [
     experiencia: '2 años',
     desc: 'Artista de la mirada con certificación',
     emoji: '💁',
-    foto: 'fotos/mafe.jpeg',
+    foto: 'fotos/mafe.png',
     tags: ['Extensiones', 'Lifting', 'Tinte']
   },
    {
